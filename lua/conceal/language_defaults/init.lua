003abc6992fc2065dcd795c8f643b560bf5e49ce
qiekn <qieknxxx@gmail.com>
2025-06-07 23:09:51 +0800

    feat: modular

--- lua/conceal/language_defaults/init.lua
+++ lua/conceal/language_defaults/init.lua
@@ -1,5 +1,6 @@
 local language_defaults = {}
 
 language_defaults["python"] = require("conceal.language_defaults.python")
+language_defaults["lua"] = require("conceal.language_defaults.lua")
 
 return language_defaults
---
+++ lua/conceal/language_defaults/lua.lua
@@ -0,0 +1,67 @@
+return {
+  ["require"] = {
+    enabled = true,
+    conceal = "r",
+    highlight = "function.builtin",
+  },
+  ["local"] = {
+    enabled = true,
+    conceal = "l",
+    highlight = "keyword",
+  },
+  ["function"] = {
+    enabled = true,
+    conceal = "func",
+    highlight = "keyword.function",
+  },
+  ["if"] = {
+    enabled = true,
+    conceal = "?",
+    highlight = "conditional",
+  },
+  ["else"] = {
+    enabled = true,
+    conceal = "e",
+    highlight = "conditional",
+  },
+  ["elseif"] = {
+    enabled = true,
+    conceal = "e",
+    highlight = "conditional",
+  },
+  ["for"] = {
+    enabled = true,
+    conceal = "F",
+    highlight = "repeat",
+  },
+  ["return"] = {
+    enabled = true,
+    conceal = "R",
+    highlight = "keyword.function",
+  },
+  ["and"] = {
+    enabled = true,
+    conceal = "&",
+    highlight = "keyword.function",
+  },
+  ["end"] = {
+    enabled = true,
+    conceal = "}",
+    highlight = "keyword.function",
+  },
+  ["then"] = {
+    enabled = true,
+    conceal = "{",
+    highlight = "conditional",
+  },
+  ["do"] = {
+    enabled = true,
+    conceal = "{",
+    highlight = "repeat",
+  },
+  ["in"] = {
+    enabled = true,
+    conceal = "i",
+    highlight = "keyword",
+  },
+}
